9d3644b720e1fdda8c08151df6a3f7cfd1895889
ruchtem <34424489+ruchtem@users.noreply.github.com>
2019-08-01 18:23:41 +0200

    First experiment with event handling

--- Zatacka/Main.cpp
+++ Zatacka/Main.cpp
@@ -1,10 +1,22 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
 
 int main()
 {
+	// Show key selecting menu
 	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+
+	// -> Register key strokes as events. Remember wich key was pressed.
+
+	// Start the game
+
+
 	sf::CircleShape shape(100.f);
 	shape.setFillColor(sf::Color::Green);
+
+	sf::Keyboard::Key pressedKey;
 
 	while (window.isOpen())
 	{
@@ -13,6 +25,18 @@ int main()
 		{
 			if (event.type == sf::Event::Closed)
 				window.close();
+
+			if (sf::Event::KeyPressed)
+				pressedKey = event.key.code;
+				std::cout << pressedKey << endl;
+		}
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
+			// left key is pressed: Change color to blue
+			shape.setFillColor(sf::Color::Blue);
+		}
+		else {
+			shape.setFillColor(sf::Color::Green);
 		}
 
 		window.clear();
